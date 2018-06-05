bc83895cb98a9fd089f191b3be5bf88fea3a371a
Manngold <36980767+Manngold@users.noreply.github.com>
2018-06-06 02:43:04 +0900

    practice blockchain
    validation blockchain

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js");
+
 class Block{
     constructor(index, hash, previousHash, timestamp, data){
     this.index = index;
@@ -18,4 +20,55 @@ const genesisBlock = new Block(
 
 let blockchain = [genesisBlock];
 
-console.log(blockchain);
+const getLastBlock = () => blockchain[blockchain - 1];
+
+const createHash = (index, previousHash, timestamp, data) =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+
+const getTimeStamp = () => new Date().getTime() / 1000;
+const creatNewBlock = data => {
+    const previousBlock = getLastBlock();
+    const newBlockIndex = previousBlock.index + 1;
+    const newTimeStamp = getTimeStamp();
+    const newHash = createHash(
+        newBlockIndex,
+        previousBlock.hash,
+        newTimeStamp,
+        data
+    );
+    const newBlock = new Block(
+     newBlockIndex,
+     newHash,
+     previousBlock.hash,
+     newTimeStamp,
+     data   
+    );
+    return newBlock;
+};
+
+const getBlockHash = (block) => createHash(block.index, block.previousHash, block.timestamp, block.data) 
+
+const isNewBlockValid = (candidateBlock, latestBlock) => {
+    if(latestBlock.index + 1 !== candidateBlock.index){
+        console.log('The candidate block dosent have a vaild index');
+        return false;
+    }else if(latestBlock.hash !== candidateBlock.previousHash){
+        console.log('The previousHash of the candidate block is net the hash of the latest block');
+        return false;
+    }else if(getBlockHash(candidateBlock) !== candidateBlock.hash){
+        console.log('The hash of this block is invalid');
+        return false;
+    }
+    return true;
+};
+
+const isNewStructureValid = (block) => {
+    return (
+        typeof block.index ==='number' &&
+        typeof block.hash === "string" &&
+        typeof block.previousHash === 'string' &&
+        typeof block.timestamp === 'number'&&
+        typeof block.data === 'string'
+    )
+}
